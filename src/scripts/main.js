aaa677723871c6469bb940ef74fce234790187ce
Dejan Stjepanovic <ds2q88@gmail.com>
2018-05-09 15:14:27 -0500

    initial push

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,11 @@
 // const listAnimals = require("./dom/DisplayCatalog")
 const orderForm = require("./dom/OrderForm")
+const commissionWork = require("./dom/CommissionWork")
 
 // Display a list of all animals
 // listAnimals()
 
 // Display order form
-orderForm(1)
+// orderForm(1)
+commissionWork(1)
 
